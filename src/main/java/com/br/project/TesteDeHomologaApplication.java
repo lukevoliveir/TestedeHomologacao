2f65697dc73ec65ca8390ebb6108037f368e7ea4
Lucas Vale <lucas.vale@original.com.br>
2019-11-16 12:11:02 -0300

    Configuração do Swagger

--- src/main/java/com/br/project/TesteDeHomologaApplication.java
+++ src/main/java/com/br/project/TesteDeHomologaApplication.java
@@ -2,9 +2,18 @@ package com.br.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableCaching
 public class TesteDeHomologaApplication {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TesteDeHomologaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteDeHomologaApplication.class, args);
